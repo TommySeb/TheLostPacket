c11378284472371f33c089e19ad9af12b50e0ab5
Tomas Sebastianelli <tomasfantabu@gmail.com>
2026-07-23 14:53:42 +0200

    Inizio implementazione ambienti
    Inizio dell'implementazione degli ambienti di gioco:
    
    1- Importate le risorse associate, ossia le immagini di sfondo e i sottofondi degli ambienti,
    2- Importato JavaFX all'interno del progetto Gradle,
    3- Creata la struttura base delle classi per l'ambiente.

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130577/ambienti/controller/AmbienteController.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.mpgc.rpg130577.ambienti.controller;
+
+public class AmbienteController {
+}
---
+++ src/main/java/it/unicam/cs/mpgc/rpg130577/ambienti/gui/AmbienteGUI.java
@@ -0,0 +1,12 @@
+package it.unicam.cs.mpgc.rpg130577.ambienti.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AmbienteGUI extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Gioco - The Lost Packet");
+        stage.show();
+    }
+}
