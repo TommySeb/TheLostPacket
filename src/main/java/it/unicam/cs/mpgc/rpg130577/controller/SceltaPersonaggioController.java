c1b97c359bf9a7c6c5ed1e6cf51083a9d75418a6
Tomas Sebastianelli <tomasfantabu@gmail.com>
2026-08-14 11:33:52 +0200

    Creati eventi per scelta personaggio
    Creati e collegati (ma non ancora implementati) eventi per la schermata di scelta del personaggio

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130577/controller/SceltaPersonaggioController.java
@@ -0,0 +1,20 @@
+package it.unicam.cs.mpgc.rpg130577.controller;
+
+import javafx.fxml.FXML;
+
+public class SceltaPersonaggioController {
+    @FXML
+    private void scegliMasterizzatore(){
+
+    }
+
+    @FXML
+    private void scegliSistemista(){
+
+    }
+
+    @FXML
+    private void scegliSniffer(){
+
+    }
+}
